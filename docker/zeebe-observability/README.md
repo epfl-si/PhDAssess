ae5d8ef0b8564143c6dbea8bb729832d5066a22e
Julien Delasoie <julien.delasoie@epfl.ch>
2025-11-14 12:00:24 +0100

    chore: First step of moving Ops files into the PhDAssess.ops repo

--- docker/zeebe-observability/README.md
+++
@@ -1,7 +0,0 @@
-# Develop
-
-```
-perl Makefile.PL
-make
-env ZEEBE_OBSERVABILITY_DATA_DIR=$PWD/../../snapshots plackup -R lib/ -r bin/app.psgi
-```
--- scripts/generateConstantsForBpmn.js
+++
@@ -1,7 +0,0 @@
-const ZB = require('zeebe-node')
-
-const processFile = './bpmn-model/phdAssessProcess.bpmn'
-
-void (async () => {
-  console.log(await ZB.BpmnParser.generateConstantsForBpmnFiles(processFile))
-})()
